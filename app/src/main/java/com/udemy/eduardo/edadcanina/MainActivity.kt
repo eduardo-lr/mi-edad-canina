2d81ba02d060a781439bce3023aa52e735e2cdb6
Eduardo Lara Ramirez <eduardo.larar@sat.gob.mx>
2021-10-25 19:19:44 -0500

    Hasta la lección 10

--- app/src/main/java/com/udemy/eduardo/edadcanina/MainActivity.kt
+++ app/src/main/java/com/udemy/eduardo/edadcanina/MainActivity.kt
@@ -2,10 +2,24 @@ package com.udemy.eduardo.edadcanina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ageEdit : EditText = findViewById<EditText>(R.id.age_edit)
+        val resultText : TextView = findViewById<TextView>(R.id.result_text)
+        val button : Button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+            val ageString : String = ageEdit.text.toString()
+            val ageInt : Int = ageString.toInt()
+            var result : Int = ageInt * 7
+            resultText.text = "Tu edad caninca es de ${result} años"
+        }
     }
 }
