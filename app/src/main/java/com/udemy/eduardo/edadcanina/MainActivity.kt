8b9405c14c47edfeb403aba5269cbcf20039f22c
Eduardo Lara Ramirez <eduardo.larar@sat.gob.mx>
2021-10-27 13:37:59 -0500

    Manejo de cadenas demasiado largas

--- app/src/main/java/com/udemy/eduardo/edadcanina/MainActivity.kt
+++ app/src/main/java/com/udemy/eduardo/edadcanina/MainActivity.kt
@@ -14,9 +14,13 @@ class MainActivity : AppCompatActivity() {
         binding.button.setOnClickListener{
             val ageString : String = binding.ageEdit.text.toString()
             try {
-                val ageInt : Int = ageString.toInt()
-                val result : Int = ageInt * 7
+                if (ageString.length > 2)
+                    throw ToLongAgeException()
+                val ageInt: Int = ageString.toInt()
+                val result: Int = ageInt * 7
                 binding.resultText.text = getString(R.string.text_result, result)
+            } catch (tlae: ToLongAgeException) {
+                Toast.makeText(this, R.string.your_age_cannot_be_to_long, Toast.LENGTH_SHORT).show()
             } catch (nfe: NumberFormatException) {
                 Toast.makeText(this, R.string.you_must_insert_your_age, Toast.LENGTH_SHORT).show()
             }
---
+++ app/src/main/java/com/udemy/eduardo/edadcanina/ToLongAgeException.kt
@@ -0,0 +1,5 @@
+package com.udemy.eduardo.edadcanina
+
+class ToLongAgeException() :
+    NumberFormatException() {
+}
